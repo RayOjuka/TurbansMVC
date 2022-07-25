30f94e0232a37fcd875cb3ddb02a1a07ae8f5a5f
Ray Ojuka <ray@pathwaysinternational.com>
2022-07-25 15:33:29 +0300

    Added Initial Migration, with Identity

---
+++ TurbansMVC/Models/Identity/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TurbansMVC.Models.Identity
+{
+    public class ApplicationUser : IdentityUser
+
+    {
+        
+        [PersonalData]
+        public string FullName { get; set; }
+        
+        [PersonalData]
+        public string JobDescription { get; set; }
+        
+        [PersonalData]
+        public DateTime? BirthDate { get; set; }
+    }
+}
